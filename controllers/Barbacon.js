7063c685e16e2152cd5bc644eba487a51f75f767
Joao Semedo <jsemedo@sendys.local>
2020-08-07 23:26:19 +0100

    Adicionado validação/id/Datecreate_at controller/

--- controllers/Barbacon.js
+++ controllers/Barbacon.js
@@ -2,13 +2,44 @@ const dataBarbeiro = require("../data.json");
 const fs = require("fs");
 
 const index = (req, res) => {
+  // for (barbeiro in dataBarbeiro.barbeiros) {
+  //   console.log(barbeiro.name);
+  // }
+
   res.status(200).json(dataBarbeiro);
 };
 
 const addBarbeiro = (req, res) => {
-  newBarbas = req.body;
+  //Recebe os dados do req.body
+  //criar objecto com os dados que proveem do req.body (RAW POSTMAN)
+  const newBarbas = Object.keys(req.body);
 
-  dataBarbeiro.barbeiros.push(newBarbas);
+  //Validação dados introduzidos atraves do POSTMAN (só funciona quando tiveremos um formulario)
+
+  for (barbas of newBarbas) {
+    if (req.body[barbas] == "") {
+      return res.send("preencha todos os campo");
+    }
+  }
+
+  //Descontruir o objeto -> para variaveis separadas
+  let { name, location, capacity, awards, telephone, email } = req.body;
+
+  //Valores automaticos
+  const id = Number(dataBarbeiro.barbeiros.length + 1);
+  const create_at = Date.now();
+
+  dataBarbeiro.barbeiros.push({
+    id,
+    name,
+    location,
+    capacity,
+    awards,
+    telephone,
+    email,
+    create_at,
+  });
+
   res.status(200).json(dataBarbeiro);
 
   fs.writeFile("data.json", JSON.stringify(dataBarbeiro, null, 2), (err) => {
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const barbeiro = require("../controllers/Barbacon");
+const barbeiro = require("../controllers/barbaCon");
 
 const routes = express.Router();
 
