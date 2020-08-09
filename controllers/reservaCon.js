f99c53f37848882e1e10b3646bb533e9b3320245
innight <joaosemedo16@hotmail.com>
2020-08-09 23:08:59 +0100

    Refatoring reservas. Right code is below (comment)

--- controllers/reservaCon.js
+++ controllers/reservaCon.js
@@ -1,5 +1,13 @@
-const dataReservasDisponiveis = require('../reservasDisponiveis.json');
-const dataReservasEfetuadas = require('../reservasEfetuadas.json');
+// // CABULAS //
+// /*Ficheiros JSON*/
+// dataDisp - Horas disponiveis para marcar
+// datasEfect - Datas das Reservas efetuadas
+// /*Nomes dos Arrays JSON*/
+// horasDisp - Horas disponiveis para marcação
+// resEfect - Reservas efetuadas
+
+const dataDisp = require('../reservasDisponiveis.json');
+const datasEfect = require('../reservasEfetuadas.json');
 
 const fs = require('fs');
 
@@ -8,44 +16,96 @@ const index = (req, res) => {
 
     let dateInput = {
         date: '11-08-2020',
-        idBarbeiro: 1,
+        idBarbeiro: 2,
     };
 
     let arrayHorasDisponiveis = [];
 
-    for (i in dataReservasEfetuadas.reservasEfetuadas) {
+    for (i in datasEfect) {
         console.log('entrou no 1 FOR');
+
         if (
-            dataReservasEfetuadas.reservasEfetuadas[i].date == dateInput.date &&
-            dataReservasEfetuadas.reservasEfetuadas[i].idBarbeiro == dateInput.idBarbeiro
+            datasEfect[i].date != dateInput.date &&
+            datasEfect[i].idBarbeiro != dateInput.idBarbeiro
         ) {
             console.log('entrou no IF');
-            //Descontruir o objeto -> para variaveis separadas
-            let { idBarbeiro, date, hour } = dataReservasEfetuadas.reservasEfetuadas[i];
 
-            //SPLICE
-            for (x in dataReservasDisponiveis.reservasDisponiveis) {
-                // console.log('entrou 2 for');
-                // console.log(dataReservasDisponiveis.reservasDisponiveis[x].hour);
+            dataDisp.forEach(function (element) {
+                arrayHorasDisponiveis.push(element.hour);
+            });
 
-                if (
-                    dataReservasDisponiveis.reservasDisponiveis[x].hour == hour &&
-                    dataReservasDisponiveis.reservasDisponiveis[x].idBarbeiro ==
-                        idBarbeiro
-                ) {
-                    dataReservasDisponiveis.reservasDisponiveis.splice(x, 1);
-                }
-                arrayHorasDisponiveis.push(
-                    dataReservasDisponiveis.reservasDisponiveis[x]
-                );
-            }
+            // //Descontruir o objeto -> para variaveis separadas
+            // let { idBarbeiro, date, hour } = datasEfect[i];
+
+            // //SPLICE - receber datas disponiveis
+            // for (x in dataDisp) {
+            //     if (dataDisp[x].hour == hour && dataDisp[x].idBarbeiro == idBarbeiro) {
+            //         dataDisp.splice(x, 1);
+            //     }
+            //     //Copiar  dataDisp.horasDis -> asrray arrayHorasDisponiveis
+            // }
         }
-        console.log(arrayHorasDisponiveis);
     }
 
-    res.status(200).json(dataReservasDisponiveis.reservasDisponiveis);
+    // dataDisp.forEach(function (element) {
+    //     arrayHorasDisponiveis.push(element.hour);
+    // });
+
+    res.status(200).json(arrayHorasDisponiveis);
 };
 
 module.exports = {
     index,
 };
+
+/*
+
+
+const dataDisp = require('../reservasDisponiveis.json');
+const datasEfect = require('../reservasEfetuadas.json');
+
+const fs = require('fs');
+
+const index = (req, res) => {
+    //VALIDAR disponibilidade da reserva
+
+    let dateInput = {
+        date: '10-08-2020',
+        idBarbeiro: 1,
+    };
+
+    let arrayHorasDisponiveis = [];
+
+    for (i in datasEfect) {
+        console.log('entrou no 1 FOR');
+        if (
+            datasEfect[i].date == dateInput.date &&
+            datasEfect[i].idBarbeiro == dateInput.idBarbeiro
+        ) {
+            console.log('entrou no IF');
+
+            //Descontruir o objeto -> para variaveis separadas
+            let { idBarbeiro, date, hour } = datasEfect[i];
+
+            //SPLICE - receber datas disponiveis
+            for (x in dataDisp) {
+                if (dataDisp[x].hour == hour && dataDisp[x].idBarbeiro == idBarbeiro) {
+                    dataDisp.splice(x, 1);
+                }
+                //Copiar  dataDisp.horasDis -> asrray arrayHorasDisponiveis
+            }
+        }
+    }
+
+    dataDisp.forEach(function (element) {
+        arrayHorasDisponiveis.push(element.hour);
+    });
+
+    res.status(200).json(arrayHorasDisponiveis);
+};
+
+module.exports = {
+    index,
+};
+
+*/
