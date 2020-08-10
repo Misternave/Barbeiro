16ba4b2684f581e1a936095dcae60153cc3da3af
innight <joaosemedo16@hotmail.com>
2020-08-10 10:03:08 +0100

    Change to functions AvaibleHours

--- controllers/reservaCon.js
+++ controllers/reservaCon.js
@@ -11,46 +11,66 @@ const datasEfect = require('../reservasEfetuadas.json');
 
 const fs = require('fs');
 
+// VARIABLES //
 const index = (req, res) => {
-    //VALIDAR disponibilidade da reserva
-
     let dateInput = {
-        date: '11-08-2020',
-        idBarbeiro: 2,
+        date: '10-08-2020',
+        idBarbeiro: 1,
     };
 
     let arrayHorasDisponiveis = [];
+    let existemReservas = false;
+    // END VARIABLES //
 
-    for (i in datasEfect) {
-        console.log('entrou no 1 FOR');
+    //FUNCTIONS//
 
-        if (
-            datasEfect[i].date != dateInput.date &&
-            datasEfect[i].idBarbeiro != dateInput.idBarbeiro
-        ) {
-            console.log('entrou no IF');
-
-            dataDisp.forEach(function (element) {
-                arrayHorasDisponiveis.push(element.hour);
-            });
-
-            // //Descontruir o objeto -> para variaveis separadas
-            // let { idBarbeiro, date, hour } = datasEfect[i];
-
-            // //SPLICE - receber datas disponiveis
-            // for (x in dataDisp) {
-            //     if (dataDisp[x].hour == hour && dataDisp[x].idBarbeiro == idBarbeiro) {
-            //         dataDisp.splice(x, 1);
-            //     }
-            //     //Copiar  dataDisp.horasDis -> asrray arrayHorasDisponiveis
-            // }
+    function carregaArrayHoras() {
+        for (x in dataDisp) {
+            arrayHorasDisponiveis.push(dataDisp[x].hour);
         }
     }
 
-    // dataDisp.forEach(function (element) {
-    //     arrayHorasDisponiveis.push(element.hour);
-    // });
+    function validaSeExistemReservas(datasEfect) {
+        for (i in datasEfect) {
+            console.log('entrou no 1 FOR - JSON - Datas efetuadas');
 
+            if (
+                datasEfect[i].date == dateInput.date &&
+                datasEfect[i].idBarbeiro == dateInput.idBarbeiro
+            ) {
+                console.log('entrou no IF');
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function mostrarHorasDisponiveis(dataDisp, datasEfect) {
+        existemReservas = validaSeExistemReservas(datasEfect);
+        carregaArrayHoras();
+        if (existemReservas === true) {
+            for (i in datasEfect) {
+                if (
+                    datasEfect[i].date === dateInput.date &&
+                    datasEfect[i].idBarbeiro == dateInput.idBarbeiro
+                ) {
+                    for (x in dataDisp) {
+                        if (datasEfect[i].hour === dataDisp[x].hour) {
+                            console.log('MATCH' + datasEfect[i].hour + ' ' + dataDisp[x].hour);
+                            arrayHorasDisponiveis.splice(x, 1);
+                        }
+                    }
+                }
+            }
+        } else {
+            for (i in dataDisp) {
+                arrayHorasDisponiveis.push(dataDisp[i].hour);
+            }
+        }
+    }
+    //END FUNCTIONS//
+
+    mostrarHorasDisponiveis(dataDisp, datasEfect);
     res.status(200).json(arrayHorasDisponiveis);
 };
 
@@ -59,7 +79,7 @@ module.exports = {
 };
 
 /*
-
+BACKUP CODE - WORKING
 
 const dataDisp = require('../reservasDisponiveis.json');
 const datasEfect = require('../reservasEfetuadas.json');
